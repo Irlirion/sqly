b5162f418afd797841c07e431e0e1b6913dc5ba1
Rostislav Grigorjev <grigorjew.rostislaw@gmail.com>
2021-10-30 12:51:20 +0300

    add tokens

--- sqly/lex.py
+++ sqly/lex.py
@@ -1,4 +1,19 @@
-import ply.lex as lex
+import ply.lex as lex, 
+from ply.lex import TOKEN
+
+
+literals = "()"
+
+reserved = {
+    "SELECT": "SELECT",
+    "DISTINCT": "DISTINCT",
+    "ALL": "ALL",
+    "FROM": "FROM",
+    "WHERE": "WHERE",
+    "NOT": "NOT",
+    "NULL": "NULL",
+    "DEFAULT": "DEFAULT",
+}
 
 tokens = [
     "SEPARATOR",
@@ -9,10 +24,22 @@ tokens = [
     "SYMBOL",
     "STRING",
     "ID",
+    "TABLE_NAME",
+    "FIELD_NAME",
     "NUMERICAL_OPERATOR",
     "NUMERICAL_FACTOR",
-    "t_NUMERICAL_EXPRESSION"
+    "NUMERICAL_EXPRESSION",
+    "VALUE",
+    "COMPARSION_OPERATOR",
+    "CONDITIONAL_OPERATOR",
+    "FIELD_VALUE",
+    "PREDICATE",
+    "CONDITIONAL_FACTOR",
+    "CONDITIONAL",
+    "FIELD_LIST",
 ]
+
+tokens += reserved.values()
 
 t_SEPARATOR = r"(\s)"
 t_PUNCTUATION_MARK = r"[\.,!?:;]"
@@ -20,11 +47,21 @@ t_DIGIT = r"\d"
 t_CHAR = r"[A-Z]"
 t_NUMBER = f"(-?{t_DIGIT}+.?{t_DIGIT}*)"
 t_SYMBOL = f"({t_DIGIT}|{t_CHAR}|{t_PUNCTUATION_MARK}|{t_SEPARATOR})"
-t_STRING = f"\"{t_SYMBOL}*\""
-t_ID = f"({t_CHAR}|_)({t_CHAR}|_|{t_DIGIT})*" 
+t_STRING = f'"{t_SYMBOL}*"'
 t_NUMERICAL_OPERATOR = r"[+\-*/]"
-# t_NUMERICAL_EXPRESSION = r"(?1)|\((?1)\)"
-# t_NUMERICAL_FACTOR = f"({t_NUMBER}|{t_NUMBER} {t_NUMERICAL_OPERATOR} {t_NUMERICAL_EXPRESSION}"
+t_COMPARSION_OPERATOR = r"(<>|<=|>=|<|>|=)"
+identifier = f"({t_CHAR}|_)({t_CHAR}|_|{t_DIGIT})*"
+
+
+def t_CONDITIONAL_OPERATOR(t):
+    r"(AND|OR)"
+    return t
+
+@TOKEN(identifier)
+def t_ID(t):
+    t.type = reserved.get(t.value, "ID")
+    return t
+
 
 def t_newline(t):
     r"\n+"
@@ -32,8 +69,16 @@ def t_newline(t):
 
 
 def t_error(t):
-    print(f"Illegal character '{t.value[0]}' on line {t.lineno} column {t.lexpos}")
+    print(
+        f"Illegal character '{t.value[0]}' on line {t.lineno} "
+        f"column {find_column(t.lexer.lexdata, t)}"
+    )
     t.lexer.skip(1)
+
+
+def find_column(input, token):
+    line_start = input.rfind("\n", 0, token.lexpos) + 1
+    return (token.lexpos - line_start) + 1
 
 
 # Build the lexer
